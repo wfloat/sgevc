e685cc04f66881b7016288a5908c9ac28c087cf7
Mitchell Sayre <sayremitch@gmail.com>
2024-07-27 20:43:00 -0400

    Rename variables

--- inference_VC.py
+++ inference_VC.py
@@ -49,33 +49,33 @@ def main():
                 hps.train.segment_size // hps.data.hop_length,
                 **hps.model).cuda()
     _ = net_g.eval()
-    _ = utils.load_checkpoint("./logs/ESD_chinese_semi_3_gamma_1.0_alpha_0.1/G_200000.pth", net_g, None)
+    _ = utils.load_checkpoint("./logs/server_training_07_25/G_20000.pth", net_g, None)
     path = Path("./filelists/ch_audio_text_val_ESD_all_fix.txt")
     with path.open('r',encoding='utf-8') as rf:
-        t = [line.split('|')[-1].strip('\n') for line in rf]
+        transcriptions = [line.split('|')[-1].strip('\n') for line in rf]
     with path.open('r',encoding='utf-8') as rf:
-        u = [line.split('|')[0].strip('\n').strip(".wav") for line in rf]
+        audio_filenames = [line.split('|')[0].strip('\n').strip(".wav") for line in rf]
     with path.open('r',encoding='utf-8') as rf:
-        s = [line.split('|')[1].strip('\n') for line in rf]
+        speaker_ids = [line.split('|')[1].strip('\n') for line in rf]
     with path.open('r',encoding='utf-8') as rf:
-        e = [line.split('|')[2].strip('\n') for line in rf]
+        emotion_ids = [line.split('|')[2].strip('\n') for line in rf]
 
     d_u_t = {}
-    for i in range(len(t)):
-        d_u_t[u[i]] = t[i]
+    for i in range(len(transcriptions)):
+        d_u_t[audio_filenames[i]] = transcriptions[i]
 
     d_u_s = {}
-    for i in range(len(t)):
-        d_u_s[u[i]] = s[i]
+    for i in range(len(transcriptions)):
+        d_u_s[audio_filenames[i]] = speaker_ids[i]
 
     d_u_e = {}
-    for i in range(len(t)):
-        d_u_e[u[i]] = e[i]
+    for i in range(len(transcriptions)):
+        d_u_e[audio_filenames[i]] = emotion_ids[i]
     
     source_wav_path = "Source/Neutral/"
     target_wav_path = "./Target/"
     speakers = ['0001','0002','0003','0004','0005','0006','0007','0008','0009','0010']
-    emotions={"生气":(351,370),"快乐":(701,720),"伤心":(1051,1070),"惊喜":(1401,1420)} 
+    emotions={"angry":(351,370),"happy":(701,720),"sad":(1051,1070),"surprised":(1401,1420)} 
  
     if not os.path.exists("./listening_test"):
         os.mkdir("./listening_test")
@@ -83,13 +83,13 @@ def main():
         os.mkdir("./listening_test/supervison_level_1")
     out = "./listening_test/supervison_level_1/"
     # do emotion conversion
-    for speaker in speakers:
+    for speaker_ids in speakers:
         for j in range(20):
             if j+1>=10:
-                wav_name_src = speaker + "_0000" + str(1+j)
+                wav_name_src = speaker_ids + "_0000" + str(1+j)
                 choose_src = source_wav_path + wav_name_src
             else:
-                wav_name_src = speaker + "_00000" + str(1+j)
+                wav_name_src = speaker_ids + "_00000" + str(1+j)
                 choose_src = source_wav_path + wav_name_src
             str_sid_src = d_u_s[f"/data/DATA/ESD-wavs-22k/{wav_name_src}"]
             spec_src = torch.load(choose_src + ".spec.pt")
@@ -98,25 +98,25 @@ def main():
             spec_src, spec_src_lengths = spec_src.cuda(), spec_src_lengths.cuda()
             
             for k,v in emotions.items():
-                if k =="生气":
+                if k =="angry":
                     out_emotion_VC = out + "N2A/"
                     out_emotion_VC_target = out + "Target_N2A/"
-                    wav_name = speaker + "_000" + str(v[0]+j)
+                    wav_name = speaker_ids + "_000" + str(v[0]+j)
                     choose_trg = target_wav_path + "Angry/" + wav_name
-                if k=="快乐":
+                if k=="happy":
                     out_emotion_VC = out + "N2H/"
                     out_emotion_VC_target = out + f"Target_N2H/"
-                    wav_name = speaker + "_000" + str(v[0]+j)
+                    wav_name = speaker_ids + "_000" + str(v[0]+j)
                     choose_trg = target_wav_path + "Happy/" + wav_name
-                if k=="伤心":
+                if k=="sad":
                     out_emotion_VC = out + "N2S1/"
                     out_emotion_VC_target = out + f"Target_N2S1/"
-                    wav_name = speaker + "_00" + str(v[0]+j)
+                    wav_name = speaker_ids + "_00" + str(v[0]+j)
                     choose_trg = target_wav_path + "Sad/" + wav_name
-                if k=="惊喜":
+                if k=="surprised":
                     out_emotion_VC = out + "N2S2/"
                     out_emotion_VC_target = out + f"Target_N2S2/"
-                    wav_name = speaker + "_00" + str(v[0]+j)
+                    wav_name = speaker_ids + "_00" + str(v[0]+j)
                     choose_trg = target_wav_path + "Surprise/" + wav_name
                 if not os.path.exists(out_emotion_VC):
                     os.mkdir(out_emotion_VC)
@@ -139,10 +139,10 @@ def main():
                     audio_vc *= 32768
                     print(f"convert the emotion of {wav_name_src} to {k}.")
                     #shutil.copy(choose_trg + ".wav", out_emotion_VC_target + f"{speaker}-{j+1}.wav")
-                    cmd = "sox "+choose_trg+".wav"+" -b 16 -e signed-integer -r 16000 "+out_emotion_VC_target+f"{speaker}-{j+1}.wav"
+                    cmd = "sox "+choose_trg+".wav"+" -b 16 -e signed-integer -r 16000 "+out_emotion_VC_target+f"{speaker_ids}-{j+1}.wav"
                     subprocess.call(cmd,shell=True)        
-                    out_path = out_emotion_VC + f"{speaker}-{j+1}.wav"
-                    out_path_16k = out_emotion_VC + f"{speaker}-{j+1}-16k.wav"
+                    out_path = out_emotion_VC + f"{speaker_ids}-{j+1}.wav"
+                    out_path_16k = out_emotion_VC + f"{speaker_ids}-{j+1}-16k.wav"
                     write(out_path, 22050, audio_vc.astype(np.int16))
                     cmd = "sox "+ out_path +" -b 16 -e signed-integer -r 16000 " + out_path_16k 
                     subprocess.call(cmd,shell=True)

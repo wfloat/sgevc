d4eca985bc91d0ba7b158d09fe486bf26e190cdc
Mitchell Sayre <sayremitch@gmail.com>
2024-09-10 23:26:45 +0000

    Finish creating the english filelists

--- filelists_en.py
+++ filelists_en.py
@@ -2,7 +2,7 @@ import phonemize
 import os
 
 data_dir = "/data/DATA/ESD-wavs-22k/"
-filelists_dir = "/filelists/"
+filelists_dir = "./filelists/english/"
 en_speaker_ids = range(11, 21)
 emotions = [
     {
@@ -60,12 +60,56 @@ train_3_path = os.path.join(filelists_dir, "en_audio_text_train_ESD_3_fix.txt")
 train_30_path = os.path.join(filelists_dir, "en_audio_text_train_ESD_30_fix.txt")
 val_all_path = os.path.join(filelists_dir, "en_audio_text_val_ESD_all_fix.txt")
 test_all_path = os.path.join(filelists_dir, "en_audio_text_test_ESD_all_fix.txt")
+val_range = range(1, 21)
+test_range = range(21, 51)
+train_labeled_1_percent = range(51, 54)
+train_labeled_10_percent = range(51, 81)
 with open(train_all_path, "w") as train_all, open(train_3_path, "w") as train_3, open(train_30_path, "w") as train_30, open(val_all_path, "w") as val_all, open(test_all_path, "w") as test_all:
     for i in en_speaker_ids:
         offset = 0
         for emotion in emotions:
             for key, value in sorted_results_dict.items():
                 key_emotion = key + offset
-                file.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|{emotion['id']}|{value}")
-                file.write("\n")
+                if key in val_range:
+                    val_all.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|{emotion['id']}|{value}")
+                    val_all.write("\n")
+                elif key in test_range:
+                    test_all.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|{emotion['id']}|{value}")
+                    test_all.write("\n")
+                else:
+                    train_all.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|{emotion['id']}|{value}")
+                    train_all.write("\n")
+                    if key in train_labeled_1_percent:
+                        train_3.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|{emotion['id']}|{value}")
+                        train_3.write("\n")
+                    else:
+                        train_3.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|100000|{value}")
+                        train_3.write("\n")
+                    if key in train_labeled_10_percent:
+                        train_30.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|{emotion['id']}|{value}")
+                        train_30.write("\n")
+                    else:
+                        train_30.write(f"{data_dir}00{i}_{key_emotion:06d}.wav|{i - 1}|100000|{value}")
+                        train_30.write("\n")
             offset += 350
+
+def count_non_100000_percentage(file_path):
+    total_rows = 0
+    non_100000_rows = 0
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            total_rows += 1
+            parts = line.strip().split('|')
+            if len(parts) > 2 and parts[2] != '100000':
+                non_100000_rows += 1
+
+    if total_rows == 0:
+        return 0  # To avoid division by zero
+
+    non_100000_percentage = (non_100000_rows / total_rows) * 100
+    return non_100000_percentage
+
+# file_path = 'filelists/ch_audio_text_train_ESD_3_fix.txt'
+# percentage = count_non_100000_percentage(file_path)
+# print(f"Percentage of rows without 100000 in the third column: {percentage:.2f}%")
